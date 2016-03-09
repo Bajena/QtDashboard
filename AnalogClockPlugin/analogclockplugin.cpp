f11d917146c28da4f837176bcff7e457cea1de10
Jan Bajena <bajena3@gmail.com>
2016-03-09 19:16:39 +0100

    Added base for drawing ticks

--- AnalogClockPlugin/analogclockplugin.cpp
+++ AnalogClockPlugin/analogclockplugin.cpp
@@ -16,21 +16,31 @@ AnalogClockPlugin::~AnalogClockPlugin()
 
 void AnalogClockPlugin::initializeScene(QGraphicsScene &scene)
 {
+    QColor hourColor(127, 0, 127);
+    QColor minuteColor(0, 127, 127, 191);
+    QColor secondColor(127, 127, 0, 191);
+
     QPolygonF hourPolygon;
     hourPolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -40);
     this->hourHand = new QGraphicsPolygonItem(hourPolygon);
+    this->hourHand->setBrush(hourColor);
     scene.addItem(this->hourHand);
-
 
     QPolygonF minutePolygon;
     minutePolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -70);
     this->minuteHand = new QGraphicsPolygonItem(minutePolygon);
+    this->minuteHand->setBrush(minuteColor);
     scene.addItem(this->minuteHand);
 
     QPolygonF secondPolygon;
     secondPolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -90);
     this->secondHand = new QGraphicsPolygonItem(secondPolygon);
+    this->secondHand->setBrush(secondColor);
     scene.addItem(this->secondHand);
+
+    for (int i = 0; i < 12; ++i) {
+        scene.addLine(88, 0, 96, 0)->setRotation(30.0);
+    }
 
     rotateHands();
 }
