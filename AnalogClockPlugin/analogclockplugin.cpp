83257a399114f614a0f5af10658605f3da814378
Jan Bajena <bajena3@o2.pl>
2016-03-10 23:42:05 +0100

    My code works, I don't know why xD

--- AnalogClockPlugin/analogclockplugin.cpp
+++ AnalogClockPlugin/analogclockplugin.cpp
@@ -16,21 +16,32 @@ AnalogClockPlugin::~AnalogClockPlugin()
 
 void AnalogClockPlugin::initializeScene(QGraphicsScene &scene)
 {
-    QPolygonF hourPolygon;
-    hourPolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -40);
-    this->hourHand = new QGraphicsPolygonItem(hourPolygon);
-    scene.addItem(this->hourHand);
-
-
-    QPolygonF minutePolygon;
-    minutePolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -70);
-    this->minuteHand = new QGraphicsPolygonItem(minutePolygon);
-    scene.addItem(this->minuteHand);
+    QColor hourColor(127, 0, 127);
+    QColor minuteColor(0, 127, 127, 191);
+    QColor secondColor(127, 127, 0, 140);
 
     QPolygonF secondPolygon;
     secondPolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -90);
     this->secondHand = new QGraphicsPolygonItem(secondPolygon);
+    this->secondHand->setBrush(secondColor);
     scene.addItem(this->secondHand);
+
+    QPolygonF minutePolygon;
+    minutePolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -70);
+    this->minuteHand = new QGraphicsPolygonItem(minutePolygon);
+    this->minuteHand->setBrush(minuteColor);
+    scene.addItem(this->minuteHand);
+
+    QPolygonF hourPolygon;
+    hourPolygon << QPointF(7, 8) << QPointF(-7, 8) << QPointF(0, -40);
+    this->hourHand = new QGraphicsPolygonItem(hourPolygon);
+    this->hourHand->setBrush(hourColor);
+    scene.addItem(this->hourHand);
+
+    for (int i = 0; i < 12; ++i) {
+        QGraphicsLineItem *line = scene.addLine(88, 0, 96, 0);
+        line->setTransform(QTransform().translate(-0.0,0).rotate(30.0 * (qreal)i).translate(0.0, 0));
+    }
 
     rotateHands();
 }
